246f9526c7b26bf052ae15f853576fabc45d952f
Vadim Makarov <add4che@gmail.com>
2021-01-03 16:35:06 +0300

    update rev

--- deps.nix
+++ deps.nix
@@ -4,8 +4,8 @@ with rec {
   async_web_scrapper = pkgs.fetchFromGitHub {
     owner = "run4w4y";
     repo = "async_web_scrapper";
-    rev = "e19e8157b9e065532bee32490e45b681f50edfae";
-    sha256 = "17m6bq53kh355l11ljjl2lzcyw7hji0x0cysa2dj3fl1d4drrj9d";
+    rev = "516669989502ccc80a54910162a9854dbf09829e";
+    sha256 = "1b4qvc5zwimcfpkhsb20z0lalgrvjsaq283mc8r8kiz2baz7lwgf";
   };
 };
 [
